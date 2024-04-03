44e8ad564dca6c0ea38fd703b6b9903f7cf83887
jagadish teki <jagadish.t@quadone.local>
2024-04-03 11:27:57 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataServiceService } from '../../api/data-service.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-node-mysql';
+  http = inject(HttpClient);
+
+  constructor( private dataService: DataServiceService){
+  }
+  
+  ngOnInit(): void{
+    this.dataService.getStudents().subscribe({
+      next: (res)=>{
+        console.log(res);
+      }
+    })
+  }
 }
